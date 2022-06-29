ba785764ec1e9fcbf4c5827ade4a2a0e56074911
stradless05 <emilymahler1@gmail.com>
2022-06-29 23:17:42 +0200

    updated javascript

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function displayTemperature(response) {
   document.getElementById(degreeElement);
   degreeElement.innerHTML = Math.round(response.data.main.temp);
-  console.log(degreeElement);
+  document.getElementById(cityElement);
+  cityElement.innerHTML = response.data.name;
+  document.getElementById(looksLike);
+  looksLike.innerHTML = response.data.weather[0].description;
+  document.getElementById(humidityElement);
+  let humidity = response.data.main.humidity;
+  console.log(humidity);
+  humidityElement.innerHTML = `Humidity: ${humidity} %`;
+  let wind = Math.round(response.data.wind.speed);
+  console.log(wind);
+  windElement.innerHTML = `Wind: ${wind} Km/H`;
 }
 
 let apiKey = "5f091c27ecd3875fabda53b65ecd4358";
