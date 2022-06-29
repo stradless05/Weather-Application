cc43d186dedeabae06fea337613f3a930f09c10d
stradless05 <emilymahler1@gmail.com>
2022-06-29 23:47:32 +0200

    updated time and day js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   document.getElementById(degreeElement);
   degreeElement.innerHTML = Math.round(response.data.main.temp);
@@ -12,6 +35,8 @@ function displayTemperature(response) {
   let wind = Math.round(response.data.wind.speed);
   console.log(wind);
   windElement.innerHTML = `Wind: ${wind} Km/H`;
+  document.getElementById(dateElement);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "5f091c27ecd3875fabda53b65ecd4358";
