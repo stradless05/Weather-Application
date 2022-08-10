6890ffca2613f68d61e4e95d8a958b1579b49465
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-08-10 18:27:57 +0200

    integrated forecast api

--- index.js
+++ index.js
@@ -22,8 +22,14 @@ function apiCall(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
   axios.get(apiUrl).then(showResponse);
 }
+
+function getForecast (coordinates) {
+  let apiKey = '5f091c27ecd3875fabda53b65ecd4358';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function showResponse(response) {
-  console.log(response);
   let cityElement = document.getElementById("cityName");
   cityElement.innerHTML = response.data.name;
   let descriptionElement = document.getElementById("looksLike");
@@ -45,6 +51,8 @@ function showResponse(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 function clickF(event) {
   let defaultTempElement = document.getElementById("temp");
@@ -100,23 +108,42 @@ function current() {
   let currentTime = document.getElementById("current");
   currentTime.innerHTML = `${day}, ${month} ${currentDate}, ${year}`;
 }
-function displayForecast () {
-  let forecastElement = document.querySelector("#forecast");
+
+function formatDay (timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+function displayForecast (response) {
+ let forecast = (response.data.daily);
+
+
+let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML= `<div class="row">`;
-let days = ["Thu", "Fri", "Sat", "Sun"];
-days.forEach(function(day) {
+
+forecast.forEach(function(forecastDay, index) {
+  if (index < 6) {
 
   forecastHTML= forecastHTML + `
   <div class="col-2">
-   <div class = "weather-forecast-date">${day}</div> 
-    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" width = "42" alt=""/>
-    <div class = "weather-forecast-temperature">
-      <span class = "weather-forecast-temperature-max">18° </span>
-      <span class = "weather-forecast-temperature-min">24°</span>
+   <div class = "weather-forecast-date">${formatDay(forecastDay.dt)}</div> 
+   <img
+   src="http://openweathermap.org/img/wn/${
+     forecastDay.weather[0].icon
+   }@2x.png"
+   alt=""
+   width="42"
+ />  
+          <div class = "weather-forecast-temperature">
+      <span class = "weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}° |
+      </span>
+      <span class = "weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
   </div>
 </div>`;
-
+  }
 })
 
 
@@ -124,4 +151,3 @@ days.forEach(function(day) {
 
 forecastElement.innerHTML=forecastHTML;
 }
-displayForecast();
